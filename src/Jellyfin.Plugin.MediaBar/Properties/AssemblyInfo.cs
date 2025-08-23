982c0f9b6df8bed63567102f36e5514d4bc271ab
IAmParadox27 <zach.ross-clyne@hotmail.co.uk>
2025-08-23 01:35:59 +0100

    Adding a framework for being able to support multiple jellyfin versions simultaneously without having to stop support for previous versions when a new one releases.

--- src/Jellyfin.Plugin.MediaBar/Properties/AssemblyInfo.cs
+++ src/Jellyfin.Plugin.MediaBar/Properties/AssemblyInfo.cs
@@ -1,6 +1,9 @@
 ﻿using System.Reflection;
+using Jellyfin.Plugin.MediaBar.Attributes;
+
 [assembly: AssemblyCompany("IAmParadox27")]
 [assembly: AssemblyProduct("Jellyfin.Plugins.MediaBar")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyTitle("Jellyfin.Plugins.MediaBar")]
 [assembly: AssemblyVersion("2.3.0.0")]
+[assembly: JellyfinVersion("10.10.7")]
